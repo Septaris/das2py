c888f0cd2f1dc5740e1302b21e3913544f84fa10
Babouh <sonny.lion@laposte.net>
2020-07-16 12:13:33 +0200

    Fix missing links for windows and update variable names

--- setup.py
+++ setup.py
@@ -1,49 +1,59 @@
 import os
-import sys
 import os.path
-from distutils.core import setup, Extension
-import numpy
+import sys
 
-sCLibDir = os.getenv("DAS2C_LIBDIR")
-sCHdrDir = os.getenv("DAS2C_INCDIR")
+import numpy
+from setuptools import Extension, setup
+
+das2c_lib_dir = os.getenv("DAS2C_LIBDIR")
+das2c_inc_dir = os.getenv("DAS2C_INCDIR")
+
+ext_library_inc = os.getenv("LIBRARY_INC", None) 
+ext_library_lib = os.getenv("LIBRARY_LIB", None) 
 	
 lDefs = []
 
-if sCLibDir: lLibDirs = [sCLibDir]
-else: lLibDirs = []
+if das2c_lib_dir: lib_dirs = [das2c_lib_dir]
+else: lib_dirs = []
 
-if sCHdrDir: lInc = [sCHdrDir, numpy.get_include()]
-else: lInc = [numpy.get_include()]
+if ext_library_lib:
+	lib_dirs.append(ext_library_lib)
 
-lSrc = ["src/_das2.c"]
+if das2c_inc_dir: lib_incs = [das2c_inc_dir, numpy.get_include()]
+else: lib_incs = [numpy.get_include()]
+
+if ext_library_inc:
+	lib_incs.append(ext_library_inc)
+
+lib_src = ["src/_das2.c"]
 
 if sys.platform.lower().startswith('sunos'):
 	ext = Extension(
-		"_das2", sources=lSrc, include_dirs=lInc, define_macros=lDefs
-		,library_dirs=lLibDirs, libraries=["das2.3","fftw3", "expat", 
+		"_das2", sources=lib_src, include_dirs=lib_incs, define_macros=lDefs
+		,library_dirs=lib_dirs, libraries=["das2.3","fftw3", "expat", 
 		                                   "ssl", "crypto", "z"]
 		,extra_compile_args=["-xc99"]
 	)
 elif sys.platform == 'win32':
-	print("setup.py: Using Headers from %s"%lInc)
-	print("setup.py: Using Libs from %s"%lLibDirs)
+	print("setup.py: Using Headers from %s"%lib_incs)
+	print("setup.py: Using Libs from %s"%lib_dirs)
 	ext = Extension(
-		"_das2", sources=lSrc, include_dirs=lInc, define_macros=lDefs
-		,library_dirs=lLibDirs, 
+		"_das2", sources=lib_src, include_dirs=lib_incs, define_macros=lDefs
+		,library_dirs=lib_dirs, 
         libraries=[
             "libdas2.3", "fftw3", "expat", "libssl", "libcrypto",
-            "zlib", "pthreadVC3", "ws2_32"
+            "zlib", "pthreadVC3", "ws2_32", "gdi32", "advapi32", "crypt32", "user32"
 		]
-        ,extra_objects=['%s/libdas2.3.a'%sCLibDir]
+        ,extra_objects=['%s/libdas2.3.lib'%das2c_lib_dir]
 	)
 
 else:
 	ext = Extension(
-		"_das2", sources=lSrc, include_dirs=lInc, define_macros=lDefs
-		,library_dirs=lLibDirs
+		"_das2", sources=lib_src, include_dirs=lib_incs, define_macros=lDefs
+		,library_dirs=lib_dirs
         ,libraries=["fftw3", "expat", "ssl", "crypto", "z"]
 		,extra_compile_args=['-std=c99', '-ggdb', '-O0']
-        ,extra_objects=['%s/libdas2.3.a'%sCLibDir]
+        ,extra_objects=['%s/libdas2.3.a'%das2c_lib_dir]
 	)
 
 
@@ -56,4 +66,3 @@ setup(description="Das2 extensions for python",
 	author_email="chris-piker@uiowa.edu",
 	url="https://das2.org/das2py"
 )
-
